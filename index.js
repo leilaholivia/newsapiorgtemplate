2c62c48b416b868c72d6c47e57b08ae2b0d66169
Leilah Hendricks <leilaholivia@gmail.com>
2017-12-01 00:38:45 +0000

    My news api project

---
+++ index.js
@@ -0,0 +1,54 @@
+/* global $  APIKEY*/
+
+$(document).ready(function(){
+  $.ajax({
+  //method: "GET",
+  url: "https://newsapi.org/v2/sources?",
+  data: { category: "Business", country:"us", language: "en", apiKey: APIKEY },
+  success: function(data){
+      if (data.status == 'ok'){
+          // alert(data.status)
+   
+          for ( var i=0 ; i< data.sources.length; i++){
+              
+              var source = document.createElement("OPTION");
+              source.setAttribute("value", data.sources[i].id);
+                source.innerHTML = data.sources[i].name;
+                document.getElementById("selection").appendChild(source);
+                
+                
+          }
+      }
+
+  }
+  })
+});
+  
+ 
+document.getElementById("submitB").onclick = function(event){
+  event.preventDefault();
+    var sourcesVar =document.getElementById("selection").value;
+    $.ajax({
+      method: "GET",
+      url: "https://newsapi.org/v2/top-headlines",
+     data: {sources:sourcesVar , apiKey: APIKEY },
+      success: function(data){
+         console.log(data.articles[0].title);
+        if (data.status == 'ok'){
+          console.log(data.articles[0].title);
+          for (var i=0; i<=5; i++){
+            console.log(data.articles[i].title);
+            var headline = document.createElement("li");
+            headline.innerHTML=data.articles[i].title;
+            document.getElementById("headlines").appendChild(headline)
+          }
+        }
+      }
+    })};
+
+
+
+
+
+
+
